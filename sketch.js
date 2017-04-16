a33803256a49088384197d2eaf5dfbe65cdd58b4
kaineko <chanodejs@yahoo.co.jp>
2017-04-17 04:46:17 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,412 @@
+window.onload = function () {
+	//要素取得列挙
+	var back = document.getElementById('back');
+	var go = document.getElementById('go');
+	var eraseAll = document.getElementById('eraseAll');
+	var black = document.getElementById('black');
+	var red = document.getElementById('red');
+	var green = document.getElementById('green');
+	var blue = document.getElementById('blue');
+	var yellow = document.getElementById('yellow');
+	var aqua = document.getElementById('aqua');
+	var purple = document.getElementById('purple');
+	var white = document.getElementById('white');
+	var superthin = document.getElementById('superthin');
+	var thin = document.getElementById('thin');
+	var middle = document.getElementById('middle');
+	var thick = document.getElementById('thick');
+	var superthick = document.getElementById('superthick');
+	var wrapper = document.getElementById('wrapper');
+	var showingPencil = document.getElementById('showingPencil');
+	var intoImg = document.getElementById('intoImg');
+	//パネルボタンの挙動設定
+	var colorSelect = document.getElementById('colorSelect');
+	var boldSelect = document.getElementById('boldSelect');
+	var colorBox = document.getElementById('colorBox');
+	var boldBox = document.getElementById('boldBox');
+	colorSelect.onclick = function(){		
+  		if(colorBox.style.display === 'block') {
+    		colorBox.style.display = 'none';
+  		} else {
+    		colorBox.style.display = 'block';
+  		}
+	}
+	boldSelect.onclick = function(){
+  		if(boldBox.style.display === 'block') {
+    		boldBox.style.display = 'none';
+  		} else {
+    		boldBox.style.display = 'block';
+  		}
+	}
+ //お絵かき張本体
+	var canvas = document.getElementById('canvas');	
+	
+	//cursor画像の初期設定
+	canvas.style.cursor = 'url(color_pencils/black95.png) 0 95,auto';
+		
+	var w = wrapper.clientWidth;
+	var h = wrapper.clientHeight;
+	canvas.width = w;
+	canvas.height = h;
+	
+	if (canvas && canvas.getContext){
+		var ctx = canvas.getContext('2d');
+		ctx.strokeStyle = '#000';
+		ctx.lineWidth =6;
+		ctx.lineCap = 'round';
+		//色の変更
+			//cursor画像変更のためのメソッド作成(色変更)
+			function getBold(){
+				var beforeColorPencil = canvas.style.cursor;
+				var boldString = beforeColorPencil.match('[0-9]{2,3}')[0];
+				return boldString;
+			}
+		black.onclick = function(){
+			canvas.style.cursor = 'url(color_pencils/black' + getBold() +'.png) 0 ' + getBold() + ',auto';
+			showingPencil.innerHTML = '<img id = "pencil-picture" src="color_pencils/black111.png">';
+			colorBox.style.display = 'none';
+			ctx.strokeStyle = '#000';
+		};
+		red.onclick = function(){
+			canvas.style.cursor = 'url(color_pencils/red' + getBold() +'.png) 0 ' + getBold() + ',auto';
+			showingPencil.innerHTML = '<img id = "pencil-picture" src="color_pencils/red111.png">';
+			colorBox.style.display = 'none';
+			ctx.strokeStyle = '#F00';
+		};
+		green.onclick = function(){
+			canvas.style.cursor = 'url(color_pencils/green' + getBold() +'.png) 0 ' + getBold() + ',auto';
+			showingPencil.innerHTML = '<img id = "pencil-picture" src="color_pencils/green111.png">';
+			colorBox.style.display = 'none';
+			ctx.strokeStyle = '#0F0';
+		};
+		blue.onclick = function(){
+			canvas.style.cursor = 'url(color_pencils/blue' + getBold() +'.png) 0 ' + getBold() + ',auto';
+			showingPencil.innerHTML = '<img id = "pencil-picture" src="color_pencils/blue111.png">';
+			colorBox.style.display = 'none';
+			ctx.strokeStyle = '#00F';
+		};
+		yellow.onclick = function(){
+			canvas.style.cursor = 'url(color_pencils/yellow' + getBold() +'.png) 0 ' + getBold() + ',auto';
+			showingPencil.innerHTML = '<img id = "pencil-picture" src="color_pencils/yellow111.png">';
+			colorBox.style.display = 'none';
+			ctx.strokeStyle = '#FF0';
+		};
+		aqua.onclick = function(){
+			canvas.style.cursor = 'url(color_pencils/aqua' + getBold() +'.png) 0 ' + getBold() + ',auto';
+			showingPencil.innerHTML = '<img id = "pencil-picture" src="color_pencils/aqua111.png">';
+			colorBox.style.display = 'none';
+			ctx.strokeStyle = '#0FF';
+		};
+		purple.onclick = function(){
+			canvas.style.cursor = 'url(color_pencils/purple' + getBold() +'.png) 0 ' + getBold() + ',auto';
+			showingPencil.innerHTML = '<img id = "pencil-picture" src="color_pencils/purple111.png">';
+			colorBox.style.display = 'none';
+			ctx.strokeStyle = '#F0F';
+		};
+		white.onclick = function(){
+			canvas.style.cursor = 'url(color_pencils/white' + getBold() +'.png) 0 ' + getBold() + ',auto';
+			showingPencil.innerHTML = '<img id = "pencil-picture" src="color_pencils/white111.png">';
+			colorBox.style.display = 'none';
+			ctx.strokeStyle = '#FFF';
+		};
+		//太さの変更
+			//cursor画像変更のためのメソッド作成(太さ変更)
+		function changeBold(boldNum){
+			var beforeBoldPencil = canvas.style.cursor;
+			var afterBoldPencil = beforeBoldPencil.replace(/[0-9]{2,3}/g,boldNum);
+			canvas.style.cursor = afterBoldPencil;
+		}
+		superthin.onclick = function(){
+			changeBold(50);
+			boldBox.style.display = 'none';
+			ctx.lineWidth = 1;
+		};
+		thin.onclick = function(){
+			changeBold(75);
+			boldBox.style.display = 'none';
+			ctx.lineWidth = 3;
+		};
+		middle.onclick = function(){
+			changeBold(95);
+			boldBox.style.display = 'none';
+			ctx.lineWidth = 6;
+		};
+		thick.onclick = function(){
+			changeBold(111);
+			boldBox.style.display = 'none';
+			ctx.lineWidth = 9;
+		};
+		superthick.onclick = function(){
+			changeBold(125);
+			boldBox.style.display = 'none';
+			ctx.lineWidth = 14;
+		};
+		//全消の実装
+		eraseAll.onclick = function(){
+			if(window.confirm('全て消します。本当によろしいですか？')){	
+			ctx.fillStyle = '#FFF';
+			ctx.fillRect(0,0,w,h);
+			pathLog.push("#FFF",h,0,h/2,[w],[h/2	]);
+			sketchLog.push(pathLog);		
+			pathLog = [];
+			}
+		};
+		//ポインタの座標を取得
+		var offsetX;
+		var offsetY;
+		canvas.addEventListener('mousedown',function(e){
+		var mouseX = e.pageX;
+		var mouseY = e.pageY;
+		var canvasRect = canvas.getBoundingClientRect();
+		var positionX = canvasRect.left + window.scrollX;
+		var positionY = canvasRect.top + window.scrollY;	
+		offsetX = mouseX - positionX;
+		offsetY = mouseY - positionY;
+		});
+		var offsetX2;
+		var offsetY2;
+		canvas.addEventListener('mousemove',function(e){
+		var mouseX2 = e.pageX;
+		var mouseY2 = e.pageY;
+		var canvasRect2 = canvas.getBoundingClientRect();
+		var positionX2 = canvasRect2.left + window.scrollX;
+		var positionY2 = canvasRect2.top + window.scrollY;	
+		offsetX2 = mouseX2 - positionX2;
+		offsetY2 = mouseY2 - positionY2;
+		});
+		var offsetX3;
+		var offsetY3;
+		canvas.addEventListener('mouseup',function(e){
+		var mouseX3 = e.pageX;
+		var mouseY3 = e.pageY;
+		var canvasRect3 = canvas.getBoundingClientRect();
+		var positionX3 = canvasRect3.left + window.scrollX;
+		var positionY3 = canvasRect3.top + window.scrollY;	
+		offsetX3 = mouseX3 - positionX3;
+		offsetY3 = mouseY3 - positionY3;
+		});
+		var offsetX4;
+		var offsetY4;
+		canvas.addEventListener('mouseout',function(e){
+		var mouseX4 = e.pageX;
+		var mouseY4 = e.pageY;
+		var canvasRect4 = canvas.getBoundingClientRect();
+		var positionX4 = canvasRect4.left + window.scrollX;
+		var positionY4 = canvasRect4.top + window.scrollY;	
+		offsetX4 = mouseX4 - positionX4;
+		offsetY4 = mouseY4 - positionY4;
+		});
+		canvas.addEventListener('mouseover',function(e){
+		var mouseX5 = e.pageX;
+		var mouseY5 = e.pageY;
+		var canvasRect5 = canvas.getBoundingClientRect();
+		var positionX5 = canvasRect5.left + window.scrollX;
+		var positionY5 = canvasRect5.top + window.scrollY;	
+		offsetX5 = mouseX5 - positionX5;
+		offsetY5 = mouseY5 - positionY5;
+		});
+		//左ボタンが押されたら描画準備
+		canvas.onmousedown = function(e){
+			ctx.beginPath();
+			ctx.moveTo(offsetX,offsetY);
+			ctx.lineTo(offsetX,offsetY);
+			lineToXLog.push(offsetX);
+			lineToYLog.push(offsetY);
+			ctx.stroke();
+		};
+		//ポインタが動いたら描画
+		canvas.onmousemove = function(e){
+			if(e.buttons === 1){
+			memoryLog = [];
+			ctx.lineTo(offsetX2,offsetY2);
+			lineToXLog.push(offsetX2);
+			lineToYLog.push(offsetY2);
+			ctx.stroke();
+			}
+		};
+		/*左ボタンが離されたらpathLogを作成し、sketchLogへ保存後削除する。
+		　また、lineToXLogとlineToYLogも初期化しておく。
+		*/
+		canvas.onmouseup = function(e){
+			pathLog.push(ctx.strokeStyle,ctx.lineWidth,offsetX,offsetY,lineToXLog,lineToYLog);
+			sketchLog.push(pathLog);
+			pathLog = [];
+			lineToXLog =[];
+			lineToYLog =[];
+		};
+		//ポインタが画面外へ出て行った時の挙動
+		canvas.onmouseout = function (e){
+			if(e.buttons === 1){
+			memoryLog = [];
+			pathLog.push(ctx.strokeStyle,ctx.lineWidth,offsetX,offsetY,lineToXLog,lineToYLog);
+			sketchLog.push(pathLog);
+			pathLog = [];
+			lineToXLog =[];
+			lineToYLog =[];
+			}
+		};
+		//ポインタが画面内へ入った時の挙動
+		canvas.onmouseover = function(e){
+			if(e.buttons === 1){
+			 ctx.beginPath();
+			 ctx.moveTo(offsetX5,offsetY5);
+			 offsetX = offsetX5;
+			 offsetY = offsetY5;
+			}
+		}
+		//戻るボタンと進むボタンの挙動
+		var sketchLog = [];//今までの全ての描写情報を保存する配列を用意
+		var pathLog = [];//1つのパスの間の描写情報を保存する配列を用意
+		var lineToXLog = [];//lineTo情報(X)を保存する配列を用意
+		var lineToYLog = [];//lineTo情報(Y)を保存する配列を用意
+		var memoryLog = [];//戻るボタンで消した描写情報を保存する配列を用意
+		back.onclick = function(){
+			if(sketchLog.length > 0){
+				ctx.save(); 
+				ctx.fillStyle = '#FFF';
+				ctx.fillRect(0,0,w,h);
+				memoryLog.push(sketchLog.pop());
+				for (var i = 0; i < sketchLog.length; i++){
+					ctx.beginPath();
+					ctx.strokeStyle = sketchLog[i][0];
+					ctx.lineWidth = sketchLog[i][1];
+					ctx.moveTo(sketchLog[i][2],sketchLog[i][3]);
+					for (var k = 0; k < sketchLog[i][4].length; k++){
+						ctx.lineTo(sketchLog[i][4][k],sketchLog[i][5][k]);
+					}
+					ctx.stroke();
+				}
+				ctx.restore();
+			}
+		}
+		go.onclick = function(){
+			if(memoryLog.length > 0){
+				ctx.save();
+				ctx.fillStyle = '#FFF';
+				ctx.fillRect(0,0,w,h);
+				sketchLog.push(memoryLog.pop());
+				for (var i = 0; i < sketchLog.length; i++){
+					ctx.beginPath();
+					ctx.strokeStyle = sketchLog[i][0];
+					ctx.lineWidth = sketchLog[i][1];
+					ctx.moveTo(sketchLog[i][2],sketchLog[i][3]);
+					for (var k = 0; k < sketchLog[i][4].length; k++){
+						ctx.lineTo(sketchLog[i][4][k],sketchLog[i][5][k]);
+					}
+					ctx.stroke();
+				}
+				ctx.restore();
+			}
+		}
+		//画像化して保存する
+		intoImg.onclick = function(){
+			ctx.fillStyle = '#FFF';
+			ctx.fillRect(0,0,w,h);
+			for (var i = 0; i < sketchLog.length; i++){
+					ctx.beginPath();
+					ctx.strokeStyle = sketchLog[i][0];
+					ctx.lineWidth = sketchLog[i][1];
+					ctx.moveTo(sketchLog[i][2],sketchLog[i][3]);
+					for (var k = 0; k < sketchLog[i][4].length; k++){
+						ctx.lineTo(sketchLog[i][4][k],sketchLog[i][5][k]);
+					}
+					ctx.stroke();
+			}
+			var imgUrl =canvas.toDataURL();	
+			window.open(imgUrl);
+		}
+		/*
+		//塗りつぶしの実装(他に、太さを4.6.8.10.14に変更)
+		document.onkeydown = function(e){
+			if(e.key === 't'){
+				rihgtCheck(offsetX,offsetY);
+				downCheck(offsetX,offsetY);
+				leftCheck(offsetX,offsetY);
+				upCheck(offsetX,offsetY);
+				function rihgtCheck(numX,numY){
+					for (var i=0; i < w; i=i+2){
+						var imagedata1 = ctx.getImageData(numX+i,numY,1,1);
+						var imagedata2 = ctx.getImageData(numX+i+2,numY,1,1);
+						if (imagedata1.data[0] === imagedata2.data[0] && imagedata1.data[1] === imagedata2.data[1] && imagedata1.data[3] === imagedata2.data[3]){
+									return false;
+						} else {
+							ctx.beginPath();
+							ctx.moveTo(numX+i,numY);
+							ctx.lineTo(numX+i+2,numY);
+							ctx.stroke();
+						}
+					};
+				}
+				function downCheck(numX,numY){
+					for (var i=0; i < w; i=i+2){
+						var imagedata1 = ctx.getImageData(numX,numY+i,1,1);
+						var imagedata2 = ctx.getImageData(numX,numY+i+2,1,1);
+						if (imagedata1.data[0] === imagedata2.data[0] && imagedata1.data[1] === imagedata2.data[1] && imagedata1.data[3] === imagedata2.data[3]){
+									return false;
+						} else {
+							ctx.beginPath();
+							ctx.moveTo(numX,numY+i);
+							ctx.lineTo(numX,numY+i+2);
+							ctx.stroke();
+						}
+					};
+				}
+				function leftCheck(numX,numY){
+					for (var i=0; i < w; i=i+2){
+						var imagedata1 = ctx.getImageData(numX-i,numY,-1,-1);
+						var imagedata2 = ctx.getImageData(numX-i-2,numY,-1,-1);
+						if (imagedata1.data[0] === imagedata2.data[0] && imagedata1.data[1] === imagedata2.data[1] && imagedata1.data[3] === imagedata2.data[3]){
+									return false;
+						} else {
+							ctx.beginPath();
+							ctx.moveTo(numX-i,numY);
+							ctx.lineTo(numX-i-2,numY);
+							ctx.stroke();
+						}
+					};
+				}
+				function upCheck(numX,numY){
+					for (var i=0; i < w; i=i+2){
+						var imagedata1 = ctx.getImageData(numX,numY-i,-1,-1);
+						var imagedata2 = ctx.getImageData(numX,numY-i-2,-1,-1);
+						if (imagedata1.data[0] === imagedata2.data[0] && imagedata1.data[1] === imagedata2.data[1] && imagedata1.data[3] === imagedata2.data[3]){
+									return false;
+						} else {
+							ctx.beginPath();
+							ctx.moveTo(numX,numY-i);
+							ctx.lineTo(numX,numY-i-2);
+							ctx.stroke();
+						}
+					};
+				}
+			}
+		}
+		*/
+		//塗りつぶしの実装（isPointInPath()メソッドを使ってみる
+		document.onkeydown = function(e){
+			if (e.key === 'f'){
+				ctx.save();
+				if(sketchLog.length > 0){
+					var s = sketchLog.length-1;
+					console.log(s);
+					ctx.beginPath();
+					console.log(sketchLog[s][0]);
+					ctx.lineWidth = 5;
+					ctx.moveTo(sketchLog[s][2],sketchLog[s][3]);
+					for (var k = 0; k < sketchLog[s][4].length; k++){
+						ctx.lineTo(sketchLog[s][4][k],sketchLog[s][5][k]);
+					}
+				}
+		 	}
+				for(var fX = 1;fX < w; fX = fX+4){
+					for(var fY = 1; fY < h; fY = fY+4){
+						if ( ctx.isPointInPath(fX,fY)){
+							ctx.lineTo(fX,fY);
+						}
+					}
+				}
+				ctx.stroke();
+				ctx.restore();
+		}
+	}
+}
